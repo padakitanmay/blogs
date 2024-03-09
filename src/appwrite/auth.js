cb7fdefbf60c078eae6df021b239fb12eceb8768
padakitanmay <padakitanmay@gmail.com>
2024-03-09 16:38:39 +0530

    auth.js was added

---
+++ src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl) // Your API Endpoint
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, pass, name }) {
+        try {
+            const userAccount = await this.account.create(
+                ID.unique(),
+                email,
+                pass,
+                name
+            );
+            if (userAccount) {
+                return this.login({ email, pass });
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({ email, pass }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, pass);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
