0022f954afe86c0da89c785147b996f52eadb6ca
padakitanmay <padakitanmay@gmail.com>
2024-03-17 11:43:43 +0530

    buttons were added

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
             <div className='w-full block'>
                 <Header />
-                HALLO 
+                HALLO
                 <Footer />
             </div>
         </div>
