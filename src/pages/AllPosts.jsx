26e4ea1ecffa034cd48f6579ed66bf534b104999
padakitanmay <padakitanmay@gmail.com>
2024-03-22 20:10:52 +0530

    as

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,24 +1,20 @@
 import React, { useState, useEffect } from "react";
 import appwriteService from "../appwrite/config";
-import { PostCard, Container } from "../components/index";
+import { Container, PostCard } from "../components";
 
 function AllPosts() {
     const [posts, setPosts] = useState([]);
-    useEffect(() => {
-        appwriteService.getPosts().then((posts) => {
-            if (posts) {
-                setPosts(posts.documents);
-            }
-        });
-    }, []);
-
+    useEffect(() => {}, []);
+    appwriteService.getPosts([]).then((posts) => {
+        if (posts) {
+            setPosts(posts.documents);
+        }
+    });
     return (
         <div className='w-full py-8'>
             <Container>
                 <div className='flex flex-wrap'>
-                    {console.log(posts)}
                     {posts.map((post) => {
-                        // console.log(post);
                         <div key={post.$id} className='p-2 w-1/4'>
                             <PostCard {...post} />
                         </div>;
